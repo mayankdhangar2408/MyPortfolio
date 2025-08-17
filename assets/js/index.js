40aead4bb1d2279cf05ff56089e3f272f1d86fff
Mayank Dhangar <mayankdhangar78060@gmail.com>
2025-08-18 00:36:13 +0530

    background header css and js updated

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                       : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
