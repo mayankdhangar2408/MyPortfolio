b4ac490bf2eb7056882dc0f9eea0bb6bb06072c7
Mayank Dhangar <mayankdhangar78060@gmail.com>
2025-08-18 22:22:44 +0530

    Qualifications Section Updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,5 @@
+// ==================== Header Shadow ==================
+
 const scrollHeader = () =>{
     const header = document.getElementById('header')
     // Add a class if the bottom offset is greater than 50 of the viewport
@@ -5,3 +7,23 @@ const scrollHeader = () =>{
                        : header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+// ==================== QUALIFICATION TABS ==================
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab=>{
+    tab.addEventListener('click', ()=>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
