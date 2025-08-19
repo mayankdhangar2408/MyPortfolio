5fd52e8d9ca2b44a3341b5b6ce38b588c237a6d7
Mayank Dhangar <mayankdhangar78060@gmail.com>
2025-08-19 21:15:55 +0530

    Portfolio Section Updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,27 @@
 // ==================== Header Shadow ==================
 
-const scrollHeader = () =>{
+const scrollHeader = () => {
     const header = document.getElementById('header')
     // Add a class if the bottom offset is greater than 50 of the viewport
-    this.scrollY >= 50 ? header.classList.add('scroll-header') 
-                       : header.classList.remove('scroll-header')
+    this.scrollY >= 50 ? header.classList.add('scroll-header')
+        : header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
 // ==================== QUALIFICATION TABS ==================
 const tabs = document.querySelectorAll('[data-target]'),
-      tabContents = document.querySelectorAll('[data-content]')
+    tabContents = document.querySelectorAll('[data-content]')
 
-tabs.forEach(tab=>{
-    tab.addEventListener('click', ()=>{
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
         const target = document.querySelector(tab.dataset.target)
 
-        tabContents.forEach(tabContent =>{
+        tabContents.forEach(tabContent => {
             tabContent.classList.remove('qualification__active')
         })
         target.classList.add('qualification__active')
 
-        tabs.forEach(tab =>{
+        tabs.forEach(tab => {
             tab.classList.remove('qualification__active')
         })
         tab.classList.add('qualification__active')
